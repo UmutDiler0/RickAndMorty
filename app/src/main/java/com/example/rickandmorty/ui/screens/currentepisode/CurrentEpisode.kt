dd118d13972776afcdc038c9748da0819c96c90a
umut <diler.2029@gmail.com>
2025-02-03 16:52:24 +0300

    CurrentLocation screen and CurrentEpisode screen completed

--- app/src/main/java/com/example/rickandmorty/ui/screens/currentepisode/CurrentEpisode.kt
+++ app/src/main/java/com/example/rickandmorty/ui/screens/currentepisode/CurrentEpisode.kt
@@ -1,8 +1,30 @@
 package com.example.rickandmorty.ui.screens.currentepisode
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rickandmorty.common.component.CustomAppBar
+import com.example.rickandmorty.ui.screens.characters.ItemListing
 
 @Composable
 fun CurrentEpisode(){
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CustomAppBar(
+            title = "Bölüm 1 Sezon 2",
+            isVisible = true
+        )
+        ItemListing()
+    }
 
+}
+@Preview(
+    showBackground = true
+)
+@Composable
+fun ShowUi(){
+    CurrentEpisode()
 }
--- app/src/main/java/com/example/rickandmorty/ui/screens/currentlocation/CurrentLocation.kt
+++ app/src/main/java/com/example/rickandmorty/ui/screens/currentlocation/CurrentLocation.kt
@@ -1,8 +1,19 @@
 package com.example.rickandmorty.ui.screens.currentlocation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.rickandmorty.common.component.CustomAppBar
 
 @Composable
 fun CurrentLocation(){
-
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CustomAppBar(
+            title = "Earth (C-137)",
+            isVisible = true
+        )
+    }
 }
